7499eddcf447819e09a216161c4a817c447f2088
DalVlad <71518602+DalVlad@users.noreply.github.com>
2020-10-05 14:17:39 +0300

    some code refactor and bug fixes

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ import msvcrt
 
 def control():
     while True:
-        while True:
-            entry = msvcrt.getch()
-            if entry == b'd':
-                start_check(1)
-                return
-            elif entry == b'a':
-                start_check(-1)
-                return
-            elif entry == b'w':
-                start_check(2)
-                return
-            elif entry == b's':
-                start_check(-2)
-                return
+        entry = msvcrt.getch()
+        entry = entry.decode('utf-8')
+        entry = str.lower(entry)
+        if entry == 'd':
+            start_check(1)
+            return
+        elif entry == 'a':
+            start_check(-1)
+            return
+        elif entry == 'w':
+            start_check(2)
+            return
+        elif entry == 's':
+            start_check(-2)
+            return
 
 
 def output():
@@ -36,7 +37,7 @@ def output():
 
 
 def number_generator():
-    while True:
+    for i in range(101):
         x = random.randint(0, 3)
         y = random.randint(0, 3)
         if play_field[x][y] == 0:
@@ -47,6 +48,7 @@ def number_generator():
             else:
                 play_field[x][y] = 4
                 return
+    print('YOU DIED')
 
 
 def shift_field(i, x):
@@ -76,89 +78,64 @@ def shift_field(i, x):
                     play_field[j - 1][i] = 0
 
 
+def sum_columns(i, j, w):
+    if play_field[w][i] != 0:
+        if play_field[w][i] == play_field[j][i]:
+            play_field[j][i] *= 2
+            play_field[w][i] = 0
+            return True
+
+
+def sum_line(i, j, w):
+    if play_field[i][w] != 0:
+        if play_field[i][w] == play_field[i][j]:
+            play_field[i][j] *= 2
+            play_field[i][w] = 0
+            return True
+
+
 def comparison(i, j, x):  # Нахождение пары в строке
     if x == 1:  # right
         for w in range(j - 1, -1, -1):
-            if play_field[i][w] != 0:
-                if play_field[i][w] == play_field[i][j]:
-                    play_field[i][j] *= 2
-                    play_field[i][w] = 0
-                    return
+            if sum_line(i, j, w):
                 return
-        return
     elif x == -1:  # left
         for w in range(j + 1, 4, 1):
-            if play_field[i][w] != 0:
-                if play_field[i][w] == play_field[i][j]:
-                    play_field[i][j] *= 2
-                    play_field[i][w] = 0
-                    return
+            if sum_line(i, j, w):
                 return
-        return
     elif x == 2:  # up
         for w in range(j + 1, 4, 1):
-            if play_field[w][i] != 0:
-                if play_field[w][i] == play_field[j][i]:
-                    play_field[j][i] *= 2
-                    play_field[w][i] = 0
-                    return
+            if sum_columns(i, j, w):
                 return
-        return
     elif x == -2:  # down
         for w in range(j - 1, -1, -1):
-            if play_field[w][i] != 0:
-                if play_field[w][i] == play_field[j][i]:
-                    play_field[j][i] *= 2
-                    play_field[w][i] = 0
-                    return
+            if sum_columns(i, j, w):
                 return
-        return
 
 
 def check(i, x):  # Перебор строк
-    if x == 1:  # right
+    if x == 1 or x == -2:  # right, down
         for j in range(3, -1, -1):
-            if play_field[i][j] != 0 and j != 0:
-                comparison(i, j, x)
-    elif x == -1:  # left
+            comparison(i, j, x)
+    elif x == -1 or x == 2:  # left, up
         for j in range(4):
-            if play_field[i][j] != 0 and j != 3:
-                comparison(i, j, x)
-    elif x == 2:  # up
-        for j in range(4):
-            if play_field[j][i] != 0 and j != 3:
-                comparison(i, j, x)
-    elif x == -2:  # down
-        for j in range(3, -1, -1):
-            if play_field[j][i] != 0 and j != 0:
-                comparison(i, j, x)
+            comparison(i, j, x)
 
 
 def start_check(x):  # Перебор столбцов
-    if x == 1:  # right
-        for i in range(4):
-            check(i, x)
-            shift_field(i, x)
-    elif x == -1:  # left
-        for i in range(4):
-            check(i, x)
-            shift_field(i, x)
-    elif x == 2:  # up
-        for i in range(4):
-            check(i, x)
-            shift_field(i, x)
-    elif x == -2:  # down
-        for i in range(4):
-            check(i, x)
-            shift_field(i, x)
+    for i in range(4):
+        check(i, x)
+        shift_field(i, x)
 
 
 play_field = [
-    [0, 2, 0, 2],
-    [0, 0, 0, 4],
-    [0, 0, 0, 2],
-    [2, 0, 0, 0]
+    [4, 0, 0, 0],
+    [2, 0, 0, 0],
+    [2, 0, 0, 0],
+    [0, 0, 0, 0]
 ]
+for f in range(4):
+    number_generator()
 
 while True:
     number_generator()
